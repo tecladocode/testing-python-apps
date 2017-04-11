457b44ffc0fc0f7eb7dd2405b14155ca297c9bab
Jose Salvatierra <jslvtr@gmail.com>
2017-04-11 18:51:17 +0100

    S02-1: first test, continued

--- section2/video_code/tests/unit/models/item_test.py
+++ section2/video_code/tests/unit/models/item_test.py
@@ -6,6 +6,12 @@ from ..base_test import BaseTest
 
 class ItemTest(BaseTest):
     def test_create_item(self):
+        # Notice this won't work with PostgreSQL, because of
+        # foreign key constraints.
+        # The store doesn't exist yet, so it would raise an error.
+        # SQLite has no foreign key constraint enforcement, so it works there.
         item = ItemModel('test', 19.99, 1)
 
         self.assertEqual(item.name, 'test')
+        self.assertEqual(item.price, 19.99)
+        self.assertEqual(item.store_id, 1)
