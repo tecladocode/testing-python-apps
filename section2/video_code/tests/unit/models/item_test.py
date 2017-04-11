e664805d202c20aa8495d8a5183ec8da215b3a97
Jose Salvatierra <jslvtr@gmail.com>
2017-04-11 18:58:41 +0100

    S02-3: added custom error messages to assertions.
    Remember to show how this works, and how error messages are seen in a terminal.

--- section2/video_code/tests/unit/models/item_test.py
+++ section2/video_code/tests/unit/models/item_test.py
@@ -12,29 +12,33 @@ class ItemTest(BaseTest):
         # SQLite has no foreign key constraint enforcement, so it works there.
         item = ItemModel('test', 19.99, 1)
 
-        self.assertEqual(item.name, 'test')
-        self.assertEqual(item.price, 19.99)
-        self.assertEqual(item.store_id, 1)
+        self.assertEqual(item.name, 'test', "The name of the item after creation does not equal the constructor argument.")
+        self.assertEqual(item.price, 19.99, "The price of the item after creation does not equal the constructor argument.")
+        self.assertEqual(item.store_id, 1, "The store_id of the item after creation does not equal the constructor argument.")
 
     def test_item_json(self):
         item = ItemModel('test', 19.99, 1)
-
-        self.assertEqual(item.json(), {
+        expected = {
             'name': 'test',
             'price': 19.99
-        })
+        }
+
+        self.assertEqual(
+            item.json(),
+            expected,
+            "The JSON export of the item is incorrect. Received {}, expected {}.".format(item.json(), expected))
     
     def test_crud(self):
         with self.app_context():
             item = ItemModel('test', 19.99, 1)
 
-            self.assertIsNone(ItemModel.find_by_name('test'))
+            self.assertIsNone(ItemModel.find_by_name('test'), "Found an item with name 'test' before save_to_db")
 
             item.save_to_db()
 
-            self.assertIsNotNone(ItemModel.find_by_name('test'))
+            self.assertIsNotNone(ItemModel.find_by_name('test'), "Did not find an item with name 'test' after save_to_db")
 
             item.delete_from_db()
 
-            self.assertIsNone(ItemModel.find_by_name('test'))
+            self.assertIsNone(ItemModel.find_by_name('test'), "Found an item with name 'test' after delete_from_db")
         
