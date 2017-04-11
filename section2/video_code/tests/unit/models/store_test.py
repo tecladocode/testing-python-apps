760870d6aae7da2ba4e9bb2012450c99c2f2e213
Jose Salvatierra <jslvtr@gmail.com>
2017-04-11 19:11:16 +0100

    S02-4: added store tests, which are very similar to the item tests

---
+++ section2/video_code/tests/unit/models/store_test.py
@@ -0,0 +1,43 @@
+"""
+Unit tests for the store model.
+"""
+from models.store import StoreModel
+from ..base_test import BaseTest
+
+class StoreTest(BaseTest):
+    def test_create_store(self):
+        # Notice this won't work with PostgreSQL, because of
+        # foreign key constraints.
+        # The store doesn't exist yet, so it would raise an error.
+        # SQLite has no foreign key constraint enforcement, so it works there.
+        store = StoreModel('test')
+
+        self.assertEqual(store.name, 'test', "The name of the store after creation does not equal the constructor argument.")
+        self.assertListEqual(store.items.all(), [], "The store's items length was not 0 even though no items were added.")
+
+    def test_store_json(self):
+        store = StoreModel('test')
+        expected = {
+            'name': 'test',
+            'items': []
+        }
+
+        self.assertEqual(
+            store.json(),
+            expected,
+            "The JSON export of the store is incorrect. Received {}, expected {}.".format(store.json(), expected))
+    
+    def test_crud(self):
+        with self.app_context():
+            store = StoreModel('test')
+
+            self.assertIsNone(StoreModel.find_by_name('test'), "Found an store with name 'test' before save_to_db")
+
+            store.save_to_db()
+
+            self.assertIsNotNone(StoreModel.find_by_name('test'), "Did not find an store with name 'test' after save_to_db")
+
+            store.delete_from_db()
+
+            self.assertIsNone(StoreModel.find_by_name('test'), "Found an store with name 'test' after delete_from_db")
+        
