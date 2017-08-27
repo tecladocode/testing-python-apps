0718bcae4c7fa3a95622c8cda7f9af66ffbc9f5f
Jose Salvatierra <jslvtr@gmail.com>
2017-08-27 18:19:10 +0100

    Fix incorrect reference to old package.

--- section3/video_code/tests/system/app_test.py
+++ section3/video_code/tests/system/app_test.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from section1.video_code import app
+from section3.video_code import app
 from blog import Blog
 from post import Post
 
