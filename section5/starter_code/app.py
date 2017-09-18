ab4b188894093d1e9ed4ea6eed2154502b0bfa54
Jose Salvatierra <jslvtr@gmail.com>
2017-09-18 14:38:05 +0100

    Finish adding starter code to section 5.

---
+++ section5/starter_code/app.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask
+from flask_restful import Api
+
+from resources.item import Item, ItemList
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+api = Api(app)
+
+api.add_resource(Item, '/item/<string:name>')
+
+if __name__ == '__main__':
+    from db import db
+
+    db.init_app(app)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
---
+++ section5/starter_code/readme.md
@@ -0,0 +1,14 @@
+# Stores REST Api
+
+This is built with Flask, Flask-RESTful, Flask-JWT, and Flask-SQLAlchemy.
+
+To get started:
+
+- Create a virtualenv for this project
+- Install requirements using `pip install -r requirements.txt`
+
+When you've created the first test, you'll also need to create a correct runtime configuration in PyCharm.
+
+Create a sample unittest configuration, and choose:
+
+- `Path` as as target, with your project's `/tests` folder.
