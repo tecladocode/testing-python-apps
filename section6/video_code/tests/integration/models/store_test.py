56d16b7958ef94d701521abc5d8e1347fd298dde
Jose Salvatierra <jslvtr@gmail.com>
2017-09-21 09:26:40 +0100

    Moved a test to integration in section6.

--- section6/video_code/tests/integration/models/store_test.py
+++ section6/video_code/tests/integration/models/store_test.py
@@ -4,6 +4,11 @@ from tests.base_test import BaseTest
 
 
 class StoreTest(BaseTest):
+    def test_create_store(self):
+        store = StoreModel('test')
+        self.assertListEqual(store.items.all(), [],
+                             "The store's items length was not 0 even though no items were added.")
+
     def test_crud(self):
         with self.app_context():
             store = StoreModel('test')
@@ -29,3 +34,15 @@ class StoreTest(BaseTest):
 
             self.assertEqual(store.items.count(), 1)
             self.assertEqual(store.items.first().name, 'test_item')
+
+    def test_store_json(self):
+        store = StoreModel('test')
+        expected = {
+            'name': 'test',
+            'items': []
+        }
+
+        self.assertEqual(
+            store.json(),
+            expected,
+            "The JSON export of the store is incorrect. Received {}, expected {}.".format(store.json(), expected))
--- section6/video_code/tests/unit/models/store_test.py
+++ section6/video_code/tests/unit/models/store_test.py
@@ -8,17 +8,3 @@ class StoreTest(BaseTest):
 
         self.assertEqual(store.name, 'test',
                          "The name of the store after creation does not equal the constructor argument.")
-        self.assertListEqual(store.items.all(), [],
-                             "The store's items length was not 0 even though no items were added.")
-
-    def test_store_json(self):
-        store = StoreModel('test')
-        expected = {
-            'name': 'test',
-            'items': []
-        }
-
-        self.assertEqual(
-            store.json(),
-            expected,
-            "The JSON export of the store is incorrect. Received {}, expected {}.".format(store.json(), expected))
